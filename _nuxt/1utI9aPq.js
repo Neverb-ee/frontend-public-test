cabad016b3e9a1d336a98e5b41bd7cec1c35b479
Neverb-ee <private_repo@github.actions>
2025-06-04 10:31:42 +0000

    See https://github.com/Neverb-ee/frontend/commit/78523627865acde548544394c078a91520aea802 from refs/heads/main

---
+++ _nuxt/1utI9aPq.js
@@ -0,0 +1,1 @@
+const t={test:{t:0,b:{t:2,i:[{t:3}],s:"pruebaaaa español"}},"About Us":{t:0,b:{t:2,i:[{t:3}],s:"Sobre Nosotros"}}};export{t as default};
---
+++ _nuxt/C14en35_.js
@@ -0,0 +1,1 @@
+const t={test:{t:0,b:{t:2,i:[{t:3}],s:"testeeoo english"}},"About Us":{t:0,b:{t:2,i:[{t:3}],s:"About Us haejejej"}}};export{t as default};

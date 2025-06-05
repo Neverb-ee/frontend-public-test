cc112d19199b32d6d4f481651e5cbdc439c2289a
Neverb-ee <private_repo@github.actions>
2025-06-05 14:46:48 +0000

    See https://github.com/Neverb-ee/frontend/commit/4ba63677d665d7e0ea9163246b1952f76759298f from refs/heads/main

---
+++ _nuxt/BuNN5vcw.js
@@ -0,0 +1,1 @@
+import{aL as e,aM as i}from"./ZY9xXlf-.js";const o=e(t=>{const a=t.params.id;if(!(typeof a=="string"&&a.length<=3&&/^[a-zA-Z0-9_-]+$/.test(a)))return i("/")});export{o as default};
--- _nuxt/CYgP9Srt.js
+++
@@ -1,1 +0,0 @@
-import{aL as e,aM as i}from"./Bs4m-E0f.js";const o=e(t=>{const a=t.params.id;if(!(typeof a=="string"&&a.length<=3&&/^[a-zA-Z0-9_-]+$/.test(a)))return i("/")});export{o as default};
